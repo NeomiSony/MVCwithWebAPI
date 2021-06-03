e919d8bb00fe14379e2483261069da314df071a6
Neomi Sony <neomisony@gmail.com>
2021-06-03 20:54:29 +0530

    Add project files.

---
+++ MVCwithWebAPI/Migrations/202106021417018_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace MVCwithWebAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Address = c.String(),
+                        Gender = c.String(),
+                        Company = c.String(),
+                        Designation = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
